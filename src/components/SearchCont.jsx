97fce8903e92f2eaa5fd9389673cad1f63b4117f
hmm365 <happyham52@gmail.com>
2022-11-24 11:10:38 +0900

    1124

--- src/components/SearchCont.jsx
+++ src/components/SearchCont.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { fetchAPI } from '../utils/fetchAPI'
+import { Videos, CateGory, Loader } from './'
 
 const SearchCont = () => {
-    return <div>SearchCont</div>
+    const [videos, setVideos] = useState(null)
+    const [selectCategory, setSelectCategory] = useState('')
+    const { searchTerm } = useParams()
+    useEffect(() => {
+        fetchAPI(`search?part=snippet&q=${searchTerm}`).then((data) => setVideos(data.items))
+    }, [searchTerm])
+    if (!videos?.length) return <Loader />
+    return (
+        <main id="main">
+            <aside id="aside">
+                <CateGory selectCategory={selectCategory} setSelectCategory={setSelectCategory} />
+            </aside>
+            <section id="contents">
+                <h2>
+                    <em>{searchTerm}</em> 를 검색하였습니다.
+                </h2>
+                <Videos videos={videos} />
+            </section>
+        </main>
+    )
 }
-
 export default SearchCont
